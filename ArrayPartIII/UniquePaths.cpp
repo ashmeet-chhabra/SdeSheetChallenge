aa6159662190d3b8474f5553743d991941e3cf73
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-06-17 13:25:47 +0530

    Update UniquePaths.cpp

--- ArrayPartIII/UniquePaths.cpp
+++ ArrayPartIII/UniquePaths.cpp
@@ -1,70 +1,31 @@
-// brute force
+#include <bits/stdc++.h> 
 
-// #include <bits/stdc++.h> 
+int uniquePaths(int m, int n, vector<vector<int>>& mat);
 
-// int uniquePaths(int i, int j, int m, int n);
+int uniquePaths(int m, int n) {
+	// Write your code here.
+    vector<vector<int>> mat(m, vector<int>(n, -1));
+    
+    return uniquePaths(m-1, n-1, mat);
+}
 
-// int uniquePaths(int m, int n) {
-// 	// Write your code here.
+int uniquePaths(int i, int j, vector<vector<int>>& mat)
+{
+    int count = 0;
     
-//     int i = 0, j = 0;
+    if(i < 0 || j < 0)
+        return 0;
     
-//     return uniquePaths(i, j, m, n);
-// }
-
-// int uniquePaths(int i, int j, int m, int n)
-// {
-//     int count = 0;
+    if(i == 0 && j == 0)
+        return 1;
     
-//     if(i >= m || j >= n)
-//         return 0;
+    if(mat[i][j] != -1)
+        return mat[i][j];
     
-//     if(i == m-1 && j == n-1)
-//         return 1;
+    count += uniquePaths(i, j - 1, mat);
+    count += uniquePaths(i - 1, j, mat);
     
-//     count += uniquePaths(i, j + 1, m, n);
-//     count += uniquePaths(i + 1, j, m, n);
+    mat[i][j] = count;
     
-//     return count;
-// }
-
-// dynamic programming
-
-// #include <bits/stdc++.h> 
-
-// int uniquePaths(int i, int j, int m, int n, vector<vector<int>> mat);
-
-// int uniquePaths(int m, int n) {
-// 	// Write your code here.
-//     int i = 0, j = 0;
-    
-//     vector<vector<int>> mat(m, vector<int>(n, -1));
-    
-//     return uniquePaths(i, j, m, n, mat);
-// }
-
-// int uniquePaths(int i, int j, int m, int n, vector<vector<int>> mat)
-// {
-//     int count = 0;
-    
-//     if(i >= m || j >= n)
-//         return 0;
-    
-//     if(i == m-1 && j == n-1)
-//         return 1;
-    
-//     if(mat[i][j] != -1)
-//         return mat[i][j];
-    
-//     count += uniquePaths(i, j + 1, m, n, mat);
-//     count += uniquePaths(i + 1, j, m, n, mat);
-    
-//     mat[i][j] = count;
-    
-//     return count;
-// }
-
-// optimal solution - combinatorics
-
-
-
+    return count;
+}
