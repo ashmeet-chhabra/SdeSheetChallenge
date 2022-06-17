5d493d43fa83ed0ada16594e85cfa64f41baf3c4
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-06-17 13:15:00 +0530

    Update UniquePaths.cpp

--- ArrayPartIII/UniquePaths.cpp
+++ ArrayPartIII/UniquePaths.cpp
@@ -28,5 +28,43 @@
 //     return count;
 // }
 
-// optimal solution
+// dynamic programming
+
+// #include <bits/stdc++.h> 
+
+// int uniquePaths(int i, int j, int m, int n, vector<vector<int>> mat);
+
+// int uniquePaths(int m, int n) {
+// 	// Write your code here.
+//     int i = 0, j = 0;
+    
+//     vector<vector<int>> mat(m, vector<int>(n, -1));
+    
+//     return uniquePaths(i, j, m, n, mat);
+// }
+
+// int uniquePaths(int i, int j, int m, int n, vector<vector<int>> mat)
+// {
+//     int count = 0;
+    
+//     if(i >= m || j >= n)
+//         return 0;
+    
+//     if(i == m-1 && j == n-1)
+//         return 1;
+    
+//     if(mat[i][j] != -1)
+//         return mat[i][j];
+    
+//     count += uniquePaths(i, j + 1, m, n, mat);
+//     count += uniquePaths(i + 1, j, m, n, mat);
+    
+//     mat[i][j] = count;
+    
+//     return count;
+// }
+
+// optimal solution - combinatorics
+
+
 
