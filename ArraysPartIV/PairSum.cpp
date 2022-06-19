071027022a8dd154a206efedcfd378af571ad7f8
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-06-20 01:51:04 +0530

    Update PairSum.cpp

--- ArraysPartIV/PairSum.cpp
+++ ArraysPartIV/PairSum.cpp
@@ -22,3 +22,25 @@ vector<vector<int>> pairSum(vector<int> &arr, int s){
 
 // optimal solution
 
+#include <vector>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < n ; ++i)
+        {
+            if(mp.find(target - nums[i]) != mp.end())
+            {
+                ans = {i, mp[target - nums[i]]};
+                return ans;
+            }
+            else
+                mp[nums[i]] = i;
+        }
+        return ans;
+    }
+};
