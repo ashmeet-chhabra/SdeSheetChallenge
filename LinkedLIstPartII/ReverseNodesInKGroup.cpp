dea93afa2405986feea101bdab04e3436ccfd6c9
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-06-25 04:37:39 +0530

    Update ReverseNodesInKGroup.cpp

--- LinkedLIstPartII/ReverseNodesInKGroup.cpp
+++ LinkedLIstPartII/ReverseNodesInKGroup.cpp
@@ -31,3 +31,50 @@ ListNode* reverseKGroup(ListNode* head, int k) {
         
         return dummy -> next;
     }
+
+// codestudio variation
+
+
+// Node *getListAfterReverseOperation(Node *head, int n, int b[]){
+// 	// Write your code here.
+//     if(n == 1)
+//         return head;
+        
+//     Node *dummy = new Node(0);
+//     dummy -> next = head;
+        
+//     Node *curr = dummy, *prev = dummy, *next = dummy;
+//     int count = 0, l = 0;
+    
+//     while(curr -> next != NULL)
+//     {
+//         curr = curr -> next;
+//         l++;
+//     }
+    
+//     while(l && (n - count))
+//     {   
+//         if(b[count] == 0)
+//             count++;
+//         else
+//         {
+//             if(l < b[count])
+//                 b[count] = l;
+        
+//             curr = prev -> next;
+//             next = curr -> next;
+//             for(int i = 1; i < b[count]; ++i)
+//             {
+//                 curr -> next = next -> next;
+//                 next -> next = prev -> next;
+//                 prev -> next = next;
+//                 next = curr -> next;
+//             }
+//             prev = curr;
+//             l -= b[count];
+//             count++;
+//         }
+//     }
+        
+//     return dummy -> next;    
+// }
