5b672e85d6e737f96c5e7b1adf465ab7ed8185b5
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-03 19:18:08 +0530

    Create RemoveDuplicatesFromSortedArray.cpp

---
+++ LinkedListAndArrays/RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,14 @@
+// brute force
+
+// int removeDuplicates(vector<int> &arr, int n) {
+// 	// Write your code here.
+//     map<int, int> mp;
+    
+//     for(int i: arr)
+//         mp[i]++;
+    
+//     return mp.size();
+// }
+
+// optimized
+
