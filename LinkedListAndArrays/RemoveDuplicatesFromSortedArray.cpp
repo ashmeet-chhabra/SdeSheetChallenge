1d2b9a871bb9aa252a297334b2b21b72a424c5e5
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-03 21:24:56 +0530

    Update RemoveDuplicatesFromSortedArray.cpp

--- LinkedListAndArrays/RemoveDuplicatesFromSortedArray.cpp
+++ LinkedListAndArrays/RemoveDuplicatesFromSortedArray.cpp
@@ -12,5 +12,17 @@
 
 // optimized (making use of the array being sorted)
 
-
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+    int count = 1;
+    
+    for(int i = 0, j = 0; (i < n && i < n); ++i)
+        if(arr[i] != arr[j])
+        {
+            count++;
+            j = i;
+        }
+    
+    return count;
+}
 
