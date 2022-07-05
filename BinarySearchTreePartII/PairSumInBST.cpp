775a24c5c5822492dfa62c80e921000f7db41537
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-06 03:40:19 +0530

    Update PairSumInBST.cpp

--- BinarySearchTreePartII/PairSumInBST.cpp
+++ BinarySearchTreePartII/PairSumInBST.cpp
@@ -1,34 +1,39 @@
-void inorder(BinaryTreeNode<int> *root, vector<int>& v);
+// brute
 
-bool pairSumBst(BinaryTreeNode<int> *root, int k)
-{
-    // Write your code here
-    vector<int> v;
-    inorder(root, v);
-    int n = v.size();
-    int i = 0, j = n - 1;
+// void inorder(BinaryTreeNode<int> *root, vector<int>& v);
+
+// bool pairSumBst(BinaryTreeNode<int> *root, int k)
+// {
+//     // Write your code here
+//     vector<int> v;
+//     inorder(root, v);
+//     int n = v.size();
+//     int i = 0, j = n - 1;
         
-    while(i < j)
-    {
-        int sum = v[i] + v[j];
-        if(sum < k)
-            i++;
-        else if(sum > k)
-            j--;
-        else
-            return true;
-    }
+//     while(i < j)
+//     {
+//         int sum = v[i] + v[j];
+//         if(sum < k)
+//             i++;
+//         else if(sum > k)
+//             j--;
+//         else
+//             return true;
+//     }
         
-    return false;
-}
+//     return false;
+// }
     
- // inorder: left node right
-void inorder(BinaryTreeNode<int> *root, vector<int>& v)
-{
-    if(root == NULL)
-        return;
+//  // inorder: left node right
+// void inorder(BinaryTreeNode<int> *root, vector<int>& v)
+// {
+//     if(root == NULL)
+//         return;
             
-    inorder(root -> left, v);
-    v.push_back(root -> data);
-    inorder(root -> right, v);
-}
+//     inorder(root -> left, v);
+//     v.push_back(root -> data);
+//     inorder(root -> right, v);
+// }
+
+// optimized solution
+
