f2040449dce00904d8490c7d472f1a2b68ba0cc1
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-06 05:05:55 +0530

    Update PairSumInBST.cpp

--- BinarySearchTreePartII/PairSumInBST.cpp
+++ BinarySearchTreePartII/PairSumInBST.cpp
@@ -37,3 +37,75 @@
 
 // optimized solution
 
+class BSTiterator
+{
+    stack<TreeNode*> stk;
+    bool reverse;
+    
+public:
+    BSTiterator(TreeNode *root, bool isReverse)
+    {
+        reverse = isReverse;
+        pushAll(root);
+    }
+
+    int next()
+    {
+        TreeNode *temp = stk.top();
+        stk.pop();
+        if(!reverse)
+            pushAll(temp -> right);
+        else
+            pushAll(temp -> left);
+        return temp -> val;
+    }
+
+    bool hasNext()
+    {
+        return !stk.empty();
+    }
+    
+    void pushAll(TreeNode* root)
+    {
+        if(reverse == true)
+        {
+            while(root != NULL)
+            {
+                stk.push(root);
+                root = root -> right;
+            }
+        }
+        else
+        {
+            while(root != NULL)
+            {
+                stk.push(root);
+                root = root -> left;
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root)
+            return false;
+        
+        BSTiterator l(root, false), r(root, true);
+        
+        int i = l.next(), j = r.next();
+        
+        while(i < j)
+        {
+            if(i + j < k)
+                i = l.next();
+            else if(i + j > k)
+                j = r.next();
+            else
+                return true;
+        }
+        
+        return false; 
+    }
+};
