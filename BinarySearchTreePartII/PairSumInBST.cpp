2a9b55c2d1cf39d1c79ffffb4fe2b5fb29f4c118
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-05 23:20:41 +0530

    Update PairSumInBST.cpp

--- BinarySearchTreePartII/PairSumInBST.cpp
+++ BinarySearchTreePartII/PairSumInBST.cpp
@@ -1,1 +1,34 @@
+void inorder(BinaryTreeNode<int> *root, vector<int>& v);
 
+bool pairSumBst(BinaryTreeNode<int> *root, int k)
+{
+    // Write your code here
+    vector<int> v;
+    inorder(root, v);
+    int n = v.size();
+    int i = 0, j = n - 1;
+        
+    while(i < j)
+    {
+        int sum = v[i] + v[j];
+        if(sum < k)
+            i++;
+        else if(sum > k)
+            j--;
+        else
+            return true;
+    }
+        
+    return false;
+}
+    
+ // inorder: left node right
+void inorder(BinaryTreeNode<int> *root, vector<int>& v)
+{
+    if(root == NULL)
+        return;
+            
+    inorder(root -> left, v);
+    v.push_back(root -> data);
+    inorder(root -> right, v);
+}
