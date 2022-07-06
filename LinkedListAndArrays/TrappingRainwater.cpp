1c74183d7318438f189b442766dae8d6fad8f36c
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-07 01:16:04 +0530

    Update TrappingRainwater.cpp

--- LinkedListAndArrays/TrappingRainwater.cpp
+++ LinkedListAndArrays/TrappingRainwater.cpp
@@ -1,23 +1,20 @@
-#include <bits/stdc++.h> 
-
 // brute 
+
 // long getTrappedWater(long *arr, int n){
     
-//     long water = 0;
-    
-//     long prefMax[n], suffMax[n];
-//     prefMax[0] = arr[0];
+//     long water = 0, a = INT_MIN;
+//     long suffMax[n];
 //     suffMax[n - 1] = arr[n - 1];
-    
-//     for(int i = 1; i < n; ++i)
-//         prefMax[i] = max(arr[i], prefMax[i - 1]);
     
 //     for(int i = n - 2; i >= 0; --i)
 //         suffMax[i] = max(arr[i], suffMax[i + 1]);
         
 //     for(int i = 0; i < n; ++i)
 //     { 
-//         long c = min(prefMax[i], suffMax[i]);
+//         if(i != 0)
+//             a = max(arr[i - 1], a);
+            
+//         long c = min(a, suffMax[i]);
         
 //         if(c > arr[i])
 //             water += c - arr[i];
