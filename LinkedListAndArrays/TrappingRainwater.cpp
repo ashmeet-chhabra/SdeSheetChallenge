8421c82bc3f04f3598aed8bbe99116573b5a193c
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-07 01:40:26 +0530

    Update TrappingRainwater.cpp

--- LinkedListAndArrays/TrappingRainwater.cpp
+++ LinkedListAndArrays/TrappingRainwater.cpp
@@ -25,3 +25,31 @@
 
 // optimized
 
+long getTrappedWater(long *arr, int n){
+    
+    long water = 0;
+    long l = 0, r = n - 1;
+    long leftMax = 0, rightMax = 0;
+        
+    while(l < r)
+    { 
+        if(arr[l] <= arr[r])
+        {
+            if(arr[l] > leftMax)
+                leftMax = arr[l];
+            else
+                water += leftMax - arr[l];
+            l++;
+        }
+        else
+        {
+            if(arr[r] > rightMax)
+                rightMax = arr[r];
+            else
+                water += rightMax - arr[r];
+            r--;
+        }
+    }
+    
+    return water;
+}
