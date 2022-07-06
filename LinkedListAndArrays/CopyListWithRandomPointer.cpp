d5d562dfb2b2a12ad79d5c8cdc008fbd58aeccc6
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-07 03:32:37 +0530

    Update CopyListWithRandomPointer.cpp

--- LinkedListAndArrays/CopyListWithRandomPointer.cpp
+++ LinkedListAndArrays/CopyListWithRandomPointer.cpp
@@ -1,1 +1,28 @@
+// brute
+
+// LinkedListNode<int> *cloneRandomList(LinkedListNode<int> *head)
+// {
+//     // Write your code here.
+//     map<LinkedListNode<int>*, LinkedListNode<int>*> mp;
+//     LinkedListNode<int> *temp = head;
+    
+//     while(temp != NULL)
+//     {
+//         mp[temp] = new LinkedListNode<int>(temp -> data);
+//         temp = temp -> next;
+//     }
+    
+//     temp = head;
+    
+//     while(temp != NULL)
+//     {
+//         mp[temp] -> next = mp[temp -> next];
+//         mp[temp] -> random = mp[temp -> random];
+//         temp = temp -> next;
+//     }
+    
+//     return mp[head];
+// }
+
+// optimized
 
