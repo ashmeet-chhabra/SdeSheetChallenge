9f8a43837035e64e22834fd8d1412d84477ee900
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-09 08:13:54 +0530

    Update MinimumNumberOfPlatforms.cpp

--- GreedyAlgorithm/MinimumNumberOfPlatforms.cpp
+++ GreedyAlgorithm/MinimumNumberOfPlatforms.cpp
@@ -58,3 +58,24 @@
 // {
 //     return a.dt < b.dt;   
 // }
+
+int calculateMinPatforms(int at[], int dt[], int n) {
+
+    sort(at, at + n);
+    sort(dt, dt + n);
+    
+    int count = 0;
+    int i = 0, j = 0;
+    
+    while(i < n && j < n)
+    {
+        if(at[i] <= dt[j])
+            count++;
+        else
+            j++;
+        
+        i++;
+    }
+    
+    return count;
+}
