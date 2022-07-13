f3da9b6c7a511c0f778bfdcfb27bc3afabba021f
ashmeet-chhabra <94653350+ashmeet-chhabra@users.noreply.github.com>
2022-07-13 06:43:45 +0530

    Update SubsetSums.cpp

--- Recursion/SubsetSums.cpp
+++ Recursion/SubsetSums.cpp
@@ -1,1 +1,23 @@
+// using bit manipulation
+// (recursion not used)
 
+vector<int> subsetSum(vector<int> &num)
+{
+    vector<int> ans;
+    int n = num.size();
+    int subsets = (1 << n);
+    
+    for(int i = 0; i < subsets; ++i)
+    {
+        int sum = 0;
+        for(int j = 0; j < n; ++j)
+        {
+            if((i & (1 << j)) != 0)
+                sum += num[j];
+        }
+        ans.push_back(sum);
+    }
+    
+    sort(ans.begin(), ans.end());
+    return ans;
+}
